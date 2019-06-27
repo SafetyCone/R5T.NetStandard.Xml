c490665d3d91a4b5e9e27a3257ce55f36f94612c
MinexAutomation <minex.automation@gmail.com>
2019-06-27 09:27:25 -0400

    Added XmlNodeExtensions and initial XPath strong-typed path types base XPath type.

---
+++ source/R5T.NetStandard.Xml/Code/Extensions/XmlNodeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace R5T.NetStandard.Xml
+{
+    public static class XmlNodeExtensions
+    {
+        public static IEnumerable<XmlNode> SelectNodesByXPath(this XmlNode xmlNode, string xPathValue)
+        {
+            var nodes = xmlNode.SelectNodes(xPathValue).OfType<XmlNode>();
+            return nodes;
+        }
+
+        public static XmlElement SelectElementByXPath(this XmlNode xmlNode, string xPathValue)
+        {
+            var xmlElement = xmlNode.SelectSingleNode(xPathValue) as XmlElement;
+            return xmlElement;
+        }
+
+        public static XElement ToXElement(this XmlNode xmlNode)
+        {
+            var xElement = XElement.Load(xmlNode.CreateNavigator().ReadSubtree());
+            return xElement;
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Xml/Code/XPaths/Classes/Base Classes/XPath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.NetStandard;
+
+
+namespace R5T.NetStandard.Xml.XPaths
+{
+    /// <summary>
+    /// Abstract base-class for all XPaths.
+    /// XPaths need not be absolute; they can be relative, they can be predicated, and they can contain axes.
+    /// </summary>
+    public abstract class XPath : TypedString
+    {
+        public XPath(string value) : base(value)
+        {
+        }
+    }
+}
