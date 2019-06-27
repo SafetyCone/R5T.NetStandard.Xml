5c168c60df3ffdc50476034cf4087e23e0225dc6
MinexAutomation <minex.automation@gmail.com>
2019-06-27 08:35:40 -0400

    Initial commit of source-stub.

---
+++ source/R5T.NetStandard.Xml.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.NetStandard.Xml.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
